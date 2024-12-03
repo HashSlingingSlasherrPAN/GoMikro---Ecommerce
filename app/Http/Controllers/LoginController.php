cd19c8c5b94a34f1a50d828d440a6825c67051c4
nopan <nofanrathe01@gmail.com>
2024-12-03 21:21:33 +0700

    rapihin pull

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
-<<<<<<< HEAD
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -39,54 +38,6 @@ class LoginController extends Controller
         // If authentication fails
         return back()->withErrors(['name' => 'Invalid credentials'])->withInput();
     }
-=======
-use Illuminate\Support\Facades\Session;
-
-class LoginController extends Controller
-{
-    function index(Request $request){
-        return view('login');
-    }
-
-    function login(Request $request){
-
-        Session::flash('email', $request->email);
-
-        $request->validate([
-            // 'name'=>'required',
-            'email' => 'required',
-            'password'=>'required'
-
-
-        ]);
-
-        $infoLogin = [
-            // 'name'=>$request->name,
-            'email' => $request->email,
-            'password'=>$request->password
-        ];
-
-           // Cek kredensial
-        if (Auth::attempt($infoLogin)) {
-            $user = Auth::user(); // Mendapatkan data pengguna yang login
-
-            // Periksa role dan arahkan ke dashboard yang sesuai
-            if ($user->role === 'admin') {
-                return redirect()->route('dashboardAdmin')->with('success', 'Login Berhasil! Selamat Datang Admin, ' . $user->name);
-            } elseif ($user->role === 'customer') {
-                return redirect()->route('dashboardCustomer')->with('success', 'Login Berhasil! Selamat Datang ' . $user->name);
-            } else {
-                // Jika role tidak dikenali
-                Auth::logout();
-                return redirect()->route('login')->with('error', 'Role pengguna tidak valid.');
-            }
-        }
-
-        // Jika kredensial salah
-        return redirect()->route('login')->with('error', 'Email atau password salah.');
-
-        }
->>>>>>> fce8b58a19512971e6eae223c7be3327743ff254
 
     public function logout(Request $request)
     {
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-<<<<<<< HEAD
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
@@ -17,35 +16,15 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/admin/dashboard', function () {
         return view('dashboardAdmin');
     })->name('adminDashboard');
-=======
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\SessionController;
-use App\Http\Controllers\Admin\ProductController;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CustomerController;
-use App\Http\Controllers\pesananBaruController;
-use App\Http\Controllers\listProdukController;
-use App\Http\Controllers\pasarkanProdukController;
-use App\Http\Controllers\RegMerchantController;
-use App\Http\Middleware\isGuest;
-use App\Http\Middleware\isLogin;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\OrderController;
-use App\Http\Controllers\LoginController;
->>>>>>> fce8b58a19512971e6eae223c7be3327743ff254
 
 });
 
 Route::middleware(['auth', 'role:customer'])->group(function () {
-  
+
     Route::get('/dashboardCustomer', [DashboardCustomerController::class, 'dashboardCustomer'])->name('dashboardCustomer');
 
 });
-<<<<<<< HEAD
 
-// Logout
-Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-=======
 Route::get('/registerMerchant', function () {
     return view('registerMerchant');
 });
@@ -53,17 +32,6 @@ Route::get('/login', function () {
     return view('login');
 });
 //Route::get('/dashbordCustomer',[CustomerController::class, 'dashboardCustomer'])->name('dashboardCustomer')->middleware([isLogin::class]);
-Route::resource('customer', CustomerController::class);
-Route::get('/session', [SessionController::class, 'index'])->name('session');
-Route::post('/logout', [SessionController::class, 'logout'])->name('logout');
-//Route::post('/login', [SessionController::class, 'login'])->name('login')->middleware(isGuest::class);
-Route::get('/profile', [SessionController::class, 'profilePage'])->name('profile');
-Route::get('/register', [SessionController::class, 'register'])->name('register');
-Route::get('/cart', [SessionController::class, 'cart'])->name('cart');
-Route::get('/registerCustomer', [SessionController::class, 'registerCustomer'])->name('registerCustomer');
-
-
-Route::post('/create', [SessionController::class, 'create'])->name('create');
 
 
 //Middleware Admin
@@ -81,4 +49,3 @@ Route::middleware('guest')->group(function () {
     Route::get('/login', [LoginController::class, 'index'])->name('login');
     Route::post('/login', [LoginController::class, 'login']);
 });
->>>>>>> fce8b58a19512971e6eae223c7be3327743ff254
